f01b96701b4e067f8dbcd4d51a285744bc85b56b
siddhartha <siddhartha1197@gmail.com>
2024-09-10 18:39:39 -0500

    feat: add intro

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './intro.css'
+
+const Intro = () => {
+  return (
+    <section id="intro">
+        <div className="introContent">
+            <span className="hello">Hello, </span>
+            <span className="introText">I'm <span className="introName">Siddhartha Alwala</span><br/>Full Stack Developer</span>
+        </div>
+        <img src="" alt="" className="bg" />
+    </section>
+  )
+}
+
+export default Intro
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbar.css'
+import {Link } from 'react-scroll'
+import contact_img from '../../assets/contact_img.png'
+
+const Navbar = () => {
+  return (
+   <nav className="navbar">
+    <div className="desktopMenu">
+      <Link  className="desktopMenuListItem" >Home</Link>
+      <Link  className="desktopMenuListItem" >Projects</Link>
+      <Link  className="desktopMenuListItem" >Experience</Link>
+      <Link  className="desktopMenuListItem" > Contact</Link>
+    </div>
+    <button div className="desktopMenuButton">
+       <img src = {contact_img} alt="" className="desktopMenuImg" /> Contact Me </button> 
+    
+   </nav>
+  )
+}
+
+export default Navbar
